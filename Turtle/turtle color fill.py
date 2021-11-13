f319d53a72aca4089a09f885b6254bab8d712d8b
Marek3008 <banasmre@gmail.com>
2021-11-13 20:39:34 +0100

    Create turtle color fill.py

---
+++ Turtle/turtle color fill.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle()
+t.fillcolor("red")
+t.begin_fill()
+def s(dlzka):
+    for i in range(4):
+      t.forward(dlzka)
+      t.left(90)
+s(50)
+
+t.end_fill()
+turtle.exitonclick()
