bada7d31fa489f805e9df6abbef2cdeef248bc5f
Marek3008 <banasmre@gmail.com>
2021-11-14 11:04:17 +0100

    commit

--- Turtle/random turtle.py
+++ Turtle/random turtle.py
@@ -6,7 +6,13 @@ t = turtle.Turtle()
 
 colormode(255)
 
-t.pencolor(randint(0,255),randint(0,255),randint(0,255))
+
+color1 = randint(0,255),randint(0,255),randint(0,255)
+
+
+t.pencolor(color1)
+t.fillcolor(color1)
+t.begin_fill()
 dlzka1 = random.randint(20,70)
 
 def stvorec(dlzka):
@@ -15,13 +21,19 @@ def stvorec(dlzka):
         t.left(90)
 stvorec(dlzka1)
 
+t.end_fill()
+
 t.penup()
 t.goto(randint(-50,200),randint(-50,200))
 t.pendown()
 
 
 
-t.pencolor(randint(0,255),randint(0,255),randint(0,255))
+color2 = randint(0,255),randint(0,255),randint(0,255)
+
+t.pencolor(color2)
+t.fillcolor(color2)
+t.begin_fill()
 dlzka2 = random.randint(20,70)
 
 def trojuholnik(dlzka):
@@ -30,13 +42,18 @@ def trojuholnik(dlzka):
       t.left(120)
 trojuholnik(dlzka2)
 
+t.end_fill()
+
 t.penup()
 t.goto(randint(-300,300),randint(-300,300))
 t.pendown()
 
 
+color3 = randint(0,255),randint(0,255),randint(0,255)
 
-t.pencolor(randint(0,255),randint(0,255),randint(0,255))
+t.pencolor(color3)
+t.fillcolor(color3)
+t.begin_fill()
 dlzka3 = random.randint(20,70)
 
 def hexagon(dlzka):
@@ -44,6 +61,8 @@ def hexagon(dlzka):
         t.forward(dlzka)
         t.left(45)
 hexagon(dlzka3)
+
+t.end_fill()
 
 
 turtle.exitonclick()
