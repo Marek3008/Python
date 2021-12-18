e8a572489124d832b800797488ede5a1cd6bad7b
Marek3008 <banasmre@gmail.com>
2021-12-18 19:14:11 +0100

    commit

---
+++ kalkulačka.py
@@ -0,0 +1,26 @@
+while True:
+       number1 = int(input("Zadaj prvé číslo: "))
+       operacia = input("Zadaj počtovú operáciu: ")
+       number2 = int(input("Zadaj druhé číslo: "))
+       vysledok = "Výsledok je "
+
+       if operacia == "+":
+              print(vysledok, number1 + number2)
+       if operacia == "-":
+              print(vysledok, number1 - number2)
+       if operacia == "*":
+              print(vysledok, number1 * number2)
+       if operacia == "/":
+              print(vysledok, number1 / number2)
+       pokracovanie = input("Chceš pokračovať? ")
+       if pokracovanie == "áno":
+              False
+       if pokracovanie == "nie":
+              break
+
+
+
+
+
+
+
