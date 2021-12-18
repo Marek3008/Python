f64a330754eb27d0dfcadedab3ebcbcc5119c951
Marek3008 <banasmre@gmail.com>
2021-12-18 17:39:13 +0100

    not mess now lulw

--- Turtle/random turtle.py
+++ Turtle/random turtle.py
@@ -1,10 +1,11 @@
 import random
 import turtle
 from turtle import*
-from random import randint
+from random import randint, randrange
 t = turtle.Turtle()
 t.hideturtle()
 t.speed(25)
+colormode(255)
 turtle.screensize(canvwidth=3000, canvheight=3000)
 "--------------------------------------------------------------"
 def after_object():
@@ -41,37 +42,35 @@ def kosostvorec(dlzka):
         t.forward(dlzka)
         t.left(110)
 "--------------------------------------------------------------"
-colormode(255)
 
 color1 = randint(0, 255), randint(0, 255), randint(0, 255)
-color_filling(color1)
+color2 = randint(0, 255), randint(0, 255), randint(0, 255)
+color3 = randint(0, 255), randint(0, 255), randint(0, 255)
+color4 = randint(0, 255), randint(0, 255), randint(0, 255)
 
 dlzka1 = random.randint(20, 70)
-stvorec(dlzka1)
-after_object()  #after štvorec
-
-color2 = randint(0, 255), randint(0, 255), randint(0, 255)
-color_filling(color2)
-
 dlzka2 = random.randint(20, 70)
-trojuholnik(dlzka2)
-after_object()  #after trojuholník
-
-color3 = randint(0, 255), randint(0, 255), randint(0, 255)
-color_filling(color3)
-
 dlzka3 = random.randint(20, 70)
-hexagon(dlzka3)
-after_object()  #after hexagon
-
-color4 = randint(0, 255), randint(0, 255), randint(0, 255)
-color_filling(color4)
-
 dlzka4 = random.randint(30, 90)
-kosostvorec(dlzka4)
-after_object()
 
-t.end_fill()
+
+for g in range(randrange(1, 4)):
+    color_filling(color1)
+    stvorec(dlzka1)
+    after_object()
+for h in range(randrange(1,4)):
+    color_filling(color2)
+    trojuholnik(dlzka2)
+    after_object()
+for u in range(randrange(1,4)):
+    color_filling(color3)
+    hexagon(dlzka3)
+    after_object()
+for z in range(randrange(1,4)):
+    color_filling(color4)
+    kosostvorec(dlzka4)
+    after_object()
+
 
 turtle.exitonclick()
 
