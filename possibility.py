28f095f76b7c0edf6f7108a23d2043cecb4556d5
Marek3008 <banasmre@gmail.com>
2021-12-18 20:56:41 +0100

    commit

--- possibility.py
+++ possibility.py
@@ -1,10 +1,15 @@
 import random
-cisla = (1,2)
+cisla = (1,2,3)
 random_numbers = random.choice(cisla)
 
 def possibility(number):
     if number == 1:
-        print("it's true")
+        print(1)
     if number == 2:
-        print("it's false")
+        print(2)
+    if number == 3:
+        print(3)
 possibility(random_numbers)
+
+
+#random choosing numbers. 33% on each number
