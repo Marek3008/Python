e25b8bd4d16b84165aad51fe6daef302b4e6f070
Marek3008 <banasmre@gmail.com>
2021-12-20 17:21:51 +0100

    commit

--- kamen papier noznice.py
+++ kamen papier noznice.py
@@ -9,36 +9,45 @@ def kmn_hra():
     while True:
         pc_tah = random.choice(tah)
         moj_tah = input("kamen/papier/noznice:  ")
-        print(pc_tah)
+        print("PC si vybral ",pc_tah)
         if moj_tah == "kamen" and pc_tah == "papier":
             pc_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "kamen" and pc_tah == "noznice":
             moje_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "kamen" and pc_tah == "kamen":
             pc_skore.append(0)
             moje_skore.append(0)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "papier" and pc_tah == "kamen":
             moje_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "papier" and pc_tah == "noznice":
             pc_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "papier" and pc_tah == "papier":
             pc_skore.append(0)
             moje_skore.append(0)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "noznice" and pc_tah == "kamen":
             pc_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "noznice" and pc_tah == "papier":
             moje_skore.append(1)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if moj_tah == "noznice" and pc_tah == "noznice":
             moje_skore.append(0)
             pc_skore.append(0)
+            print(sum(moje_skore),":", sum(pc_skore))
             False
         if sum(moje_skore) == 3:
             print("Vyhral si! ")
