0d81ea6985be37ac6908b866591bf1435bb4de01
Marek3008 <banasmre@gmail.com>
2021-12-24 12:31:06 +0100

    c

---
+++ binarna kalkulacka.py
@@ -0,0 +1,67 @@
+cislo = int(input("Zadaj číslo: "))
+binarna_forma = []
+zapisovanie = cislo
+zapisovanie128 = zapisovanie - 128
+zapisovanie64 = zapisovanie - 64
+zapisovanie32 = zapisovanie - 32
+zapisovanie16 = zapisovanie - 16
+zapisovanie8 = zapisovanie - 8
+zapisovanie4 = zapisovanie - 4
+zapisovanie2 = zapisovanie - 2
+nove_cislo = [] #2
+
+
+
+
+
+
+
+
+"""
+while True:
+    if cislo >= 128:
+        cislo - 128
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 64 and cislo < 128:
+        cislo - 64
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 32 and cislo < 64:
+        cislo - 32
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 16 and cislo < 32:
+        cislo - 16
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 8 and cislo < 16:
+        cislo - 8
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 4 and cislo < 8:
+        cislo - 4
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 2 and cislo < 4:
+        cislo - 2                           #tu si skoncil pri sum-e a nevies ako mas urobit syntax
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    if cislo >= 1 and cislo < 2:
+        cislo - 1
+        binarna_forma.append(1)
+    else:
+        binarna_forma.append(0)
+    break
+print(binarna_forma)                     
+"""         
+
+
+    
