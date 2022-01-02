6fc556087d4711f8ab795cda39a3235d0f8d1c14
Marek3008 <banasmre@gmail.com>
2022-01-02 18:58:51 +0100

    c

--- kamen papier noznice.py
+++ kamen papier noznice.py
@@ -6,7 +6,9 @@ pc_skore = []
 
 def stav():
     print(sum(moje_skore),":", sum(pc_skore))
-
+def after_tah():
+    stav()
+    False
 def kmn_hra():
     while True:
         pc_tah = random.choice(tah)
@@ -14,43 +16,34 @@ def kmn_hra():
         print("PC si vybral ",pc_tah)
         if moj_tah == "kamen" and pc_tah == "papier":
             pc_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "kamen" and pc_tah == "noznice":
             moje_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "kamen" and pc_tah == "kamen":
             pc_skore.append(0)
             moje_skore.append(0)
-            stav()
-            False
+            after_tah()
         if moj_tah == "papier" and pc_tah == "kamen":
             moje_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "papier" and pc_tah == "noznice":
             pc_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "papier" and pc_tah == "papier":
             pc_skore.append(0)
             moje_skore.append(0)
-            stav()
-            False
+            after_tah()
         if moj_tah == "noznice" and pc_tah == "kamen":
             pc_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "noznice" and pc_tah == "papier":
             moje_skore.append(1)
-            stav()
-            False
+            after_tah()
         if moj_tah == "noznice" and pc_tah == "noznice":
             moje_skore.append(0)
             pc_skore.append(0)
-            stav()
-            False
+            after_tah()
         if sum(moje_skore) == 3:
             print("Vyhral si! ")
             break
