7c7d3aae3ec6d70b620142229722cfa3ab885443
Marek3008 <banasmre@gmail.com>
2022-02-21 17:16:31 +0100

    mriezka je hotova

--- Pyglet/piskvorky-pygame.py
+++ Pyglet/piskvorky-pygame.py
@@ -1,10 +1,33 @@
 import pygame, sys
 
+
+#toto tu musi byt
 pygame.init()
+
+#konstanty
 VYSKA = 600
 SIRKA = 600
+
+#obrazovka
 obrazovka = pygame.display.set_mode((SIRKA, VYSKA))
+obrazovka.fill((93, 173, 226))
+
+#definicia ciar
+def ciara(bod_A, bod_B):
+    pygame.draw.line(obrazovka, (46, 134, 193), bod_A, bod_B, 10)
+
+#1. vertikalna
+ciara((200, 0), (200, 600))
+#2. vertikalna
+ciara((400, 0), (400, 600))
+#1. horizontalna
+ciara((0, 200), (600, 200))
+#2. horizontalna
+ciara((0, 400), (600, 400))
+
+#hlavny cyklus ktory tu tiez musi byt
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
             sys.exit()
+    pygame.display.update()
