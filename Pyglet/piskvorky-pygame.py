d8c6960f6464e1ea2cf6579f273d014c70013540
Marek3008 <banasmre@gmail.com>
2022-02-22 18:52:14 +0100

    vykresluju sa objekty ale nie presne do policok

--- Pyglet/piskvorky-pygame.py
+++ Pyglet/piskvorky-pygame.py
@@ -9,6 +9,12 @@ pygame.init()
 VYSKA = 600
 SIRKA = 600
 stlacene = False
+hrac = 1
+
+hracia_plocha = [[0,0,0],
+[0, 0, 0],
+[0, 0, 0]]
+
 
 #obrazovka
 obrazovka = pygame.display.set_mode((SIRKA, VYSKA))
@@ -27,7 +33,16 @@ ciara((0, 200), (600, 200))
 #2. horizontalna
 ciara((0, 400), (600, 400))
 
-#hlavny cyklus ktory tu tiez musi byt
+#nakresli krizik alebo kruh podla toho ktory hrac je na rade
+def nakresli_znaky():
+    if hrac == 1:
+        pygame.draw.line(obrazovka, (0, 0, 0), (pozicia_x - 80, pozicia_y - 80), (pozicia_x + 80, pozicia_y + 80), 3)
+        pygame.draw.line(obrazovka, (0, 0, 0), (pozicia_x + 80, pozicia_y - 80), (pozicia_x - 80, pozicia_y + 80), 3)
+    if hrac == -1:
+        pygame.draw.circle(obrazovka, (0, 0, 0), (pozicia_x, pozicia_y), 90, 3)
+
+
+#hlavny cyklus ktory tu tiez musi byt, v nom je vlastne napisane to co sa ma diat
 while True:
     for udalost in pygame.event.get():
         if udalost.type == pygame.QUIT:
@@ -35,6 +50,13 @@ while True:
         if udalost.type == pygame.MOUSEBUTTONDOWN and stlacene == False :
             stlacene = True
             print("Stlacene!!")
+            pozicia = pygame.mouse.get_pos()
+            pozicia_x = pozicia[0]
+            pozicia_y = pozicia[1]
+            nakresli_znaky()
+            print(pozicia)
         if udalost.type == pygame.MOUSEBUTTONUP and stlacene == True:
             stlacene = False
+            hrac *= -1
+            print(hrac)
     pygame.display.update()
