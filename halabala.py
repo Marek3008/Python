0f595c34bd868e6489626cd3ddbfe5eb60403eca
Marek3008 <banasmre@gmail.com>
2022-02-23 10:35:17 +0100

    gg

---
+++ halabala.py
@@ -0,0 +1,25 @@
+
+def ocekuj(cislo, kto_hra):
+    if kto_hra == -1:
+        kto_hra = 2
+    if cislo % 3 == 0:
+        if cislo % 5 == 0:
+            print(str(kto_hra),": HalaBala")
+        else:
+            print(str(kto_hra), ": Hala")
+    elif cislo % 5 == 0:
+        if cislo % 3 == 0:
+            print(str(kto_hra), ": HalaBala")
+        else:
+            print(str(kto_hra), ": Bala")
+    else:
+        print(str(kto_hra), ": ", cislo)
+
+n = 1
+hrac = 1
+while n < 101:
+    ocekuj(n, hrac)
+    n += 1
+    hrac *= -1
+
+    
